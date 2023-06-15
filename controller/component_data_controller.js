69a4fb24105c67d3cdf01c130cc543e493d65220
iamscj <91722818+iamscj@users.noreply.github.com>
2023-06-15 22:28:00 +0530

    vcu details api

--- controller/component_data_controller.js
+++ controller/component_data_controller.js
@@ -16,6 +16,7 @@ export const getTCDetails = async (req, res) => {
         res.json(error);
     }
 }
+
 export const getTCDetailsForConverter = async (req, res) => {
     console.log("getTCDetailsForConverter");
     const converter_no = req.params['converter_no'];
@@ -47,5 +48,48 @@ export const getTCDetailsForConverter = async (req, res) => {
     }
 };
 
+export const getACDetails = async (req, res) => {
+    console.log("getACDetails");
+    try {
+        let data = await pool.query(
+            `SELECT
+            converter_no,
+            date
+            FROM
+            ac`
+        );
+        return res.json(data.rows);
+    } catch (error) {
+        res.json(error);
+    }
+}
+export const getACDetailsForConverter = async (req, res) => {
+    console.log("getACDetailsForConverter");
+    const converter_no = req.params['converter_no'];
+    try {
+        let data = await pool.query(
+            `SELECT cr_1, cr_2, buffer_1, inverter_1, aci_1, buffer_2, inverter_2, aci_2, battery_charger FROM ac WHERE converter_no =  $1`,
+            [converter_no]
+        );
+        return res.json(data.rows);
+    } catch (error) {
+        console.log(error)
+        res.json(error);
+    }
+}
 
-
+export const getVCUDetails = async (req, res) => {
+    console.log("getVCUDetails");
+    try {
+        let data = await pool.query(
+            `SELECT
+            module_no,
+            date
+            FROM
+            vcu`
+        );
+        return res.json(data.rows);
+    } catch (error) {
+        res.json(error);
+    }
+}
--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { getTCDetails, getTCDetailsForConverter } from "../controller/component_data_controller.js";
+import { getACDetails, getACDetailsForConverter, getTCDetails, getTCDetailsForConverter, getVCUDetails } from "../controller/component_data_controller.js";
 const router = express.Router();
 
 router.get("/tc-details/:converter_no", getTCDetailsForConverter)
 router.get("/tc-details", getTCDetails)
+router.get("/ac-details", getACDetails)
+router.get("/ac-details/:converter_no", getACDetailsForConverter)
+router.get("/vcu-details", getVCUDetails)
 
 export default router;
